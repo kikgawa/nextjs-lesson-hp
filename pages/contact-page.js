b378ca6077f996655ec6f4f73f1c05b57e657845
Minori Kikugawa <minocolor@gmail.com>
2021-03-03 21:42:31 +0900

    change to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,14 +1,20 @@
-import Layout from '../components/Layout';
-import Image from 'next/image';
+import Layout from "../components/Layout";
+import Image from "next/image";
 
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title="Contact">
       <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
         <div className="mt-4">
           <p className="font-bold">Contact Info</p>
           <div className="flex justify-center mt-4">
-            <Image className="rounded-full" src="/avatar.jpg" width={60} height={60} alt="Avatar" />
+            <Image
+              className="rounded-full"
+              src="/avatar.jpg"
+              width={60}
+              height={60}
+              alt="Avatar"
+            />
           </div>
         </div>
         <div className="mt-4">
@@ -73,5 +79,4 @@ const Contact = () => {
       </div>
     </Layout>
   );
-};
-export default Contact;
+}
